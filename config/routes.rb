6f8b73c6ae60ead27d79f8e1f80e0ec48e689dcd
yk.takaha4 <yk.takaha4@gmail.com>
2018-08-24 13:58:34 +0000

    user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  root to: 'toppages#index'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:show, :new, :create]
 end

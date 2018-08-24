7d9d99431cf940ba163fec76ed7d6b25499a2c7c
yk.takaha4 <yk.takaha4@gmail.com>
2018-08-24 19:07:32 +0000

    have

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
-　　　　before_action :require_user_logged_in, only: [:show]
+  before_action :require_user_logged_in, only: [:show]
 
   def show
     @user = User.find(params[:id])
     @items = @user.items.uniq
     @count_want = @user.want_items.count
+    @count_have = @user.have_items.count
   end
 
   def new
